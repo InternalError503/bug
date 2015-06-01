dee937ef6a85745bcd01060da9627b2f54b4c432
toady <support@8pecxstudios.com>
2015-06-01 19:00:19 +1000

    Released: Bug
    Initial version.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,39 +1,10 @@
-// Copyright (c) 2014 8pecxstudios.com 
+// Copyright (c) 2015 8pecxstudios.com 
 
 if (typeof gBugMe == "undefined") {var gBugMe = {};};
 if (!gBugMe) {gBugMe = {};};
 
-let formatedURL = "";
-
 var gBugMe = {
 
-		init: function(e){
-		
-		try{
-
-		//Add click event listener to check for text selection in all elements not input fields.
-			window.addEventListener("click", function(){
-			
-			var _Numbers_Selected = document.commandDispatcher.focusedWindow.getSelection().toString();
-				//If the selection is all numbers and less then or equal to 8 it will construct the url and launch it in a new tab.
-				if(_Numbers_Selected){
-					var _is_num = /^\d+$/.test(_Numbers_Selected);
-						if (_is_num && _Numbers_Selected.length <= 8) {
-							formatedURL = 'https://bugzilla.mozilla.org/show_bug.cgi?id='+_Numbers_Selected+'';
-							document.commandDispatcher.focusedWindow.getSelection().removeAllRanges();							
-							gBrowser.selectedTab = openUILinkIn(formatedURL, 'tab');
-						}					
-				} 
-			}, false);
-			
-		
-		}catch (e){
-			//Catch any nasty errors and output to dialogue and console
-			alert("Were sorry but something has gone wrong with 'Bug initialize event' " + e);
-		}
-		
-		},
-	  //Copies current tab url to clipboard	
 		goToBug : function(e) {
 
 	    try{	
@@ -48,26 +19,23 @@ var gBugMe = {
 			_is_Transient.getTransferData("text/unicode", clipboard_str, clipboard_strLength);	
 			
 			if (clipboard_str) {
-				var _pastetext = clipboard_str.value.QueryInterface(Ci.nsISupportsString).data;
-				var _is_num = /^\d+$/.test(_pastetext);
-				
-				if (_is_num && _pastetext.length <= 8) {
-						formatedURL = 'https://bugzilla.mozilla.org/show_bug.cgi?id='+_pastetext+'';
-					if (content.location.href === "about:home" ||
-							content.location.href === "about:newtab"){
-						content.location.href = formatedURL;
-					}else{
-						gBrowser.selectedTab = openUILinkIn(formatedURL, 'tab');
+				var pastetext = clipboard_str.value.QueryInterface(Ci.nsISupportsString).data;
+				var isnum = /^\d+$/.test(pastetext);
+					if (isnum && pastetext.length <= 8) {
+						var formatedURL = 'https://bugzilla.mozilla.org/show_bug.cgi?id='+pastetext+'';			  
+						if (content.location.href === "about:home" ||
+								content.location.href === "about:newtab"){
+							content.location.href = formatedURL;
+						}else{
+							gBrowser.selectedTab = openUILinkIn(formatedURL, 'tab');
+						}
 					}
-				}
-					
 			}
 			
 			}catch (e){
 				//Catch any nasty errors and output to dialogue and console
 				alert("Were sorry but something has gone wrong with 'Bug button event' " + e);
 		}			
-	}	
-}
+	}
 
-window.addEventListener("load", function () { gBugMe.init(); }, false);
+}
