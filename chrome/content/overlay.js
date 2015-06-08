efd61d37ed90b4d2480443fbcc41febccd974b34
toady <support@8pecxstudios.com>
2015-06-08 17:32:21 +1000

    Rebuilt bug to make it more functional.
    Removed: Copying to clipboard before navigating.
    Added: Context menu entry.
    Bumped version.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -8,21 +8,11 @@ var gBugMe = {
 		goToBug : function(e) {
 
 	    try{	
-		
-			var _is_Transient = Cc["@mozilla.org/widget/transferable;1"].createInstance(Ci.nsITransferable);
-			_is_Transient.addDataFlavor("text/unicode");
-			Services.clipboard.getData(_is_Transient, Services.clipboard.kGlobalClipboard);
-
-			var clipboard_str = {};
-			var clipboard_strLength = {};
-
-			_is_Transient.getTransferData("text/unicode", clipboard_str, clipboard_strLength);	
-			
-			if (clipboard_str) {
-				var pastetext = clipboard_str.value.QueryInterface(Ci.nsISupportsString).data;
-				var isnum = /^\d+$/.test(pastetext);
-					if (isnum && pastetext.length <= 8) {
-						var formatedURL = 'https://bugzilla.mozilla.org/show_bug.cgi?id='+pastetext+'';			  
+			var aNumber = document.commandDispatcher.focusedWindow.getSelection().toString();
+			if (aNumber) {	
+				var isnum = /^\d+$/.test(aNumber);
+					if (isnum && aNumber.length <= 8) {
+						var formatedURL = 'https://bugzilla.mozilla.org/show_bug.cgi?id='+aNumber+'';			  
 						if (content.location.href === "about:home" ||
 								content.location.href === "about:newtab"){
 							content.location.href = formatedURL;
@@ -34,7 +24,7 @@ var gBugMe = {
 			
 			}catch (e){
 				//Catch any nasty errors and output to dialogue and console
-				alert("Were sorry but something has gone wrong with 'Bug button event' " + e);
+				alert("Were sorry but something has gone wrong with 'Bug go to event' " + e);
 		}			
 	}
 
